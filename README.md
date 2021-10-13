264d947c2258356bf6a30b6d9bab634be36f3367
Stephen Souness <Sounie@users.noreply.github.com>
2021-10-13 20:57:16 +1300

    Trying out accessing a Redis service from python.

---
+++ README.md
@@ -0,0 +1,1 @@
+Trying out interacting with Redis from Python.
---
+++ main.py
@@ -0,0 +1,20 @@
+from testcontainers.redis import RedisContainer
+
+if __name__ == '__main__':
+    print("starting")
+
+    with RedisContainer("redis:6.2.6-alpine") as redisContainer:
+        redisClient = redisContainer.get_client().client()
+        before = redisClient.exists("foo")
+
+        print(f"Exist before? {before}")
+
+        redisClient.set("foo", "bar")
+
+        after = redisClient.exists("foo")
+
+        print(f"Exist after? {after}")
+
+        value = redisClient.get("foo")
+
+        print(f"Value after set {value}")
